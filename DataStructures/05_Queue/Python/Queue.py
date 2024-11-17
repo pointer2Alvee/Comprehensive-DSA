132a8f7bd7471cba1e2bf1bb1f5dac3eeef65e89
pointer2Alvee <pointer2alvee@gmail.com>
2024-11-17 23:52:26 +0600

    #11, New folder Organized

---
+++ DataStructures/05_Queue/Python/Queue.py
@@ -0,0 +1,29 @@
+"""
+© alvee 2024
+email : pointer2alvee@gmail.com 
+
+Comprehensive Stack Operations/Algorithms :
+-------------------------------------------
+** Three Types of Queue Creation 
+    (A) Stack python List - No Size limit
+    (B) Stack python List - Size limit
+    (C) Stack Linked List 
+    
+All three have the following operations : 
+(1) Stack-Class
+(2) is_empty()
+(3) is_Full() 
+(4) push()
+(5) pop() 
+(6) peek()
+(7) delete() 
+
+"""
+
+
+############### (C) Queue Linked List ###############################
+#####################################################################
+
+
+################## Queue implementations #################
+##########################################################
